4c34a6a360ea69b403b481f1fbeca732baede816
Marius Antonsen <antonsen.marius@gmail.com>
2023-09-09 02:07:16 +0200

    fixed an oversight
    Signed-off-by: Marius Antonsen <antonsen.marius@gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"mime/multipart"
 	"net/http"
 	"os"
 	"time"
@@ -30,20 +27,6 @@ type PrinterStatus struct {
 }
 
 func main() {
-	// Check if the script was provided with a file argument
-	if len(os.Args) != 2 {
-		fmt.Println("Error: Usage: ./printer-monitor <file_path>")
-		return // Exit the script if the argument count is incorrect
-	}
-
-	filePath := os.Args[1] // Get the file path from the command-line argument
-
-	// Check if the provided file exists
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		fmt.Println("Error: The provided file does not exist.")
-		return // Exit the script if the file does not exist
-	}
-
 	// Print the monitoring message with the polling interval
 	fmt.Printf("Monitoring printers with a polling interval of %s...\n", pollingInterval)
 
@@ -52,8 +35,19 @@ func main() {
 		"https://ender.local.antnsn.dev/printer/info", // Add the new status URL here
 	}
 
-	// Infinite loop for continuous monitoring
 	for {
+		// Check if a file argument is provided
+		if len(os.Args) == 2 {
+			filePath := os.Args[1] // Get the file path from the command-line argument
+
+			// Check if the provided file exists
+			if _, err := os.Stat(filePath); !os.IsNotExist(err) {
+				// Process the file and send it to a printer
+				fmt.Printf("Processing file '%s'...\n", filePath)
+				processFile(filePath)
+			}
+		}
+
 		// Iterate through the list of printer status URLs
 		for _, statusURL := range statusURLs {
 			// Send a GET request to the API endpoint to get printer status
@@ -79,8 +73,7 @@ func main() {
 
 			// Check if the printer is in the "ready" state
 			if infoResponse.Result.State == "ready" {
-				processReadyPrinter(infoResponse.Result, statusURL, filePath)
-				// No need to return; we want to continue monitoring other printers
+				processReadyPrinter(infoResponse.Result, statusURL)
 			}
 		}
 
@@ -90,67 +83,15 @@ func main() {
 }
 
 // processReadyPrinter processes the action for a printer with state "ready"
-func processReadyPrinter(printer PrinterStatus, statusURL string, filePath string) {
+func processReadyPrinter(printer PrinterStatus, statusURL string) {
 	// Implement your action here, e.g., send a file to OctoPrint with 'printer.Hostname'
 	fmt.Printf("Printer with hostname '%s' is 'ready'. Taking action...\n", printer.Hostname)
 	// Add your code to send a file to OctoPrint or perform any other action for this printer
-
-	// Example: Send the file specified in the command-line argument to OctoPrint
-	sendFileToOctoPrint(printer, filePath)
 }
 
-// sendFileToOctoPrint sends a file to OctoPrint
-func sendFileToOctoPrint(printer PrinterStatus, filePath string) {
-	// Open the file for reading
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-
-	// Create a buffer for the file content
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-
-	// Create a form field for the file
-	part, err := writer.CreateFormFile("file", filePath)
-	if err != nil {
-		fmt.Println("Error creating form file:", err)
-		return
-	}
-
-	// Copy the file content to the form field
-	_, err = io.Copy(part, file)
-	if err != nil {
-		fmt.Println("Error copying file content:", err)
-		return
-	}
-
-	// Close the multipart writer
-	writer.Close()
-
-	// Create a POST request to send the file to OctoPrint
-	octoPrintURL := fmt.Sprintf("http://%s/api/files/local", printer.Hostname) // Change the URL as needed
-	req, err := http.NewRequest("POST", octoPrintURL, body)
-	if err != nil {
-		fmt.Println("Error creating POST request:", err)
-		return
-	}
-
-	// Set the API key as a header (replace 'YOUR_API_KEY' with your OctoPrint API key)
-	req.Header.Set("X-Api-Key", "YOUR_API_KEY")
-
-	// Set the Content-Type header
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-
-	// Send the request
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		fmt.Println("Error sending file to OctoPrint:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	fmt.Printf("File sent to OctoPrint on printer '%s'\n", printer.Hostname)
+// processFile processes a provided file and takes action
+func processFile(filePath string) {
+	// Implement your action here for the provided file
+	// Example: Send the file to a printer
+	fmt.Printf("File '%s' processed and sent to a printer.\n", filePath)
 }
